213308ea96c21e562759d6de6edfac82e341692b
Diogo Silva <dvsdevx@gmail.com>
2022-10-03 18:41:00 -0300

    Criacao da base do projeto

---
+++ script.js
@@ -0,0 +1,29 @@
+const b1 = 1;
+const b2 = 2;
+const b3 = 3; 
+const b4 = 4;
+const b5 = 5; 
+const b6 = 6;
+const b7 = 7;
+const b8 = 8;
+const b9 = 9;
+const b0 = 0;
+
+var valor1, valor2, result;
+
+function somar(){
+    
+   result = valor1 + valor2;
+}
+
+function multiplicar(){
+    result = valor1 * valor2;
+}
+
+function dividir(){
+    result = valor1 / valor2;
+}
+
+function subtrair(){
+    result = valor1 - valor2;
+}
